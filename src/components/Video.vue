64370b3d45a752f711f02da1d7fbea393bae5dd8
Blaz <blazyy@gmail.com>
2021-11-19 14:34:48 +0100

    videoId's added to Home.vue; minor changes in others

---
+++ src/components/Video.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Video</h1>
+  </div>
+</template>
+
+<script>
+//URL: https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=APIKEY&part=snippet,contentDetails,statistics,status
+export default {
+  name: "Video",
+};
+</script>
+
+<style></style>
